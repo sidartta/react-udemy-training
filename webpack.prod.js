70965074006161605518b01961798011b4e48462
sidartta <mohamed-amine.khalf@hec.ca>
2022-01-08 00:02:58 -0500

    Fix issue with env vars definition in webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { webpack } = require('webpack');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -13,5 +14,33 @@ module.exports = merge(common, {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(
+        process.env.FIREBASE_API_KEY
+      ),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(
+        process.env.FIREBASE_AUTH_DOMAIN
+      ),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(
+        process.env.FIREBASE_DATABASE_URL
+      ),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(
+        process.env.FIREBASE_PROJECT_ID
+      ),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
+        process.env.FIREBASE_STORAGE_BUCKET
+      ),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
+        process.env.FIREBASE_MESSAGING_SENDER_ID
+      ),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(
+        process.env.FIREBASE_APP_ID
+      ),
+      'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(
+        process.env.FIREBASE_MEASUREMENT_ID
+      ),
+    }),
+  ],
 });
